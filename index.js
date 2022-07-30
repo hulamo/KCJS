71f9b89aa26b0cf68ed4c91ce5fa1096e7f11bd3
hulamo <hugoladislao@gmail.com>
2022-07-30 06:37:35 -0700

    Programa terminado falta limpiar

--- index.js
+++ index.js
@@ -130,9 +130,9 @@ function DesplegarJuego(jugador1,jugador2,resultado1,resultado2,ganador){
     console.log("Juego --> ", jugador1,"[",resultado1,"] vs ",jugador2,"[",resultado2,"] ganador =>",ganador)
 }
 
-/// JUEGOS SEMIFINALES
+/// JUEGOS CUARTOS DE FINAL
 
-console.log("Empiezan Juegos Semifinales")
+console.log("Empiezan Juegos Cuartos de Final")
 console.log("===========================")
 
 //Juego 1
@@ -166,7 +166,7 @@ ganador=jugador1
 DesplegarJuego(jugador1,jugador2,resultado1,resultado2,ganador)
 
 //Juego 3
-let Semi_3=[]
+//let Semi_3=[]
 jugador1=Grupo_C[0]
 jugador2=Grupo_D[1]
 resultados=match()
@@ -174,15 +174,15 @@ resultado1=resultados[0]
 resultado2=resultados[1]
 if (resultado1>resultado2){
     ganador=jugador1
-Semi_3=[jugador1]
+Semi_1.push(jugador1)
 } else {
     ganador=jugador2
-    Semi_3 = [jugador2]}
+    Semi_1.push(jugador2)}
 DesplegarJuego(jugador1,jugador2,resultado1,resultado2,ganador)
 
 
 //Juego 4
-let Semi_4=[]
+//let Semi_4=[]
 jugador1=Grupo_C[1]
 jugador2=Grupo_D[0]
 resultados=match()
@@ -190,13 +190,103 @@ resultado1=resultados[0]
 resultado2=resultados[1]
 if (resultado1>resultado2){
     ganador=jugador1
-Semi_4.push(jugador1)
+Semi_2.push(jugador1)
 } else {
     ganador=jugador2
-    Semi_4.push(jugador2)}
+    Semi_2.push(jugador2)}
+DesplegarJuego(jugador1,jugador2,resultado1,resultado2,ganador)
+
+/// JUEGOS SEMIFNALES
+
+console.log ("")
+console.log ("Empiezan Juegos Semifinales")
+console.log ("============================")
+
+//Juego 1
+let Final=[]
+let Terceroy4=[]
+jugador1=Semi_1[0]
+jugador2=Semi_1[1]
+resultados=match()
+resultado1=resultados[0]
+resultado2=resultados[1]
+if (resultado1>resultado2){
+ganador=jugador1
+    Final.push(jugador1)
+    Terceroy4.push(jugador2)
+} else {
+    ganador=jugador2
+    Final.push(jugador2)
+    Terceroy4.push(jugador1)}
+DesplegarJuego(jugador1,jugador2,resultado1,resultado2,ganador)
+
+// Juego 2
+jugador1=Semi_2[0]
+jugador2=Semi_2[1]
+resultados=match()
+resultado1=resultados[0]
+resultado2=resultados[1]
+if (resultado1>resultado2){
+ganador=jugador1
+    Final.push(jugador1)
+    Terceroy4.push(jugador2)
+} else {
+    ganador=jugador2
+    Final.push(jugador2)
+    Terceroy4.push(jugador1)}
 DesplegarJuego(jugador1,jugador2,resultado1,resultado2,ganador)
 
 
+/// Empieza Juego de Tercer y Cuarto Lugar
+
+console.log ("")
+console.log ("Empiezan Juego Tercer y Cuarto Lugar")
+console.log ("============================")
+//Juego 1
+let tercero=""
+let cuarto=""
+jugador1=Terceroy4[0]
+jugador2=Terceroy4[1]
+resultados=match()
+resultado1=resultados[0]
+resultado2=resultados[1]
+if (resultado1>resultado2){
+tercero=jugador1
+cuarto=jugador2
+
+
+} else {
+    tercero=jugador2
+    cuarto=jugador1}
+DesplegarJuego(jugador1,jugador2,resultado1,resultado2,ganador)
+console.log("Cuarto Lugar del Campeonato => ",cuarto )
+console.log("Tercer Lugar del Campeonato => ",tercero )
+
+console.log ("")
+console.log ("Empieza la gran Final")
+console.log ("============================")
+//Juego 1
+let primer=""
+let segundo=""
+jugador1=Final[0]
+jugador2=Final[1]
+resultados=match()
+resultado1=resultados[0]
+resultado2=resultados[1]
+if (resultado1>resultado2){
+primer=jugador1
+segundo=jugador2
+
+
+} else {
+    primer=jugador2
+    segundo=jugador1}
+DesplegarJuego(jugador1,jugador2,resultado1,resultado2,ganador)
+console.log("Segundo Lugar del Campeonato => ",segundo )
+console.log (" ")
+console.log ("============================")
+console.log("EL GRAN CAMPEÓN !!! => ",primer )
+console.log ("============================")
 
 //console.log("Resultado1 ", resultado1)
 //console.log("Resultado2 ", resultado2)
