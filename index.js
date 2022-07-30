8a8303498b6578087bf24c3cb9c7cdbad57cdb9a
hulamo <hugoladislao@gmail.com>
2022-07-29 23:04:49 -0700

    Ya están las semifinales

--- index.js
+++ index.js
@@ -98,17 +98,106 @@ console.log("Grupo_D " ,Grupo_D)
 
 console.log(" ")
 
+
+let goles=0
+let resultado1=0
+let resultado2=0
+let resultados=[]
+
+function match(){
 let seguir =1
-while (seguir===1){
-let goles=Math.floor(Math.random() * 10) + 1
-let resultado1=Math.floor(Math.random() * goles)
-let resultado2=goles-resultado1
-if (resultado1 != resulrado2){
+    let rresultado1=0
+let rresultado2=0
+    while (seguir==1){
+
+goles=Math.floor(Math.random() * 6) + 2
+rresultado1=Math.floor(Math.random() * goles)
+rresultado2=goles-resultado1
+
+if (rresultado1 != rresultado2){
     seguir=0
 }
+}return [rresultado1,rresultado2]}
+
+
+
+/// Juegos Cuartos de Final
+let jugador1=""
+let jugador2=""
+let ganador=""
+
+function DesplegarJuego(jugador1,jugador2,resultado1,resultado2,ganador){
+    console.log("Juego --> ", jugador1,"[",resultado1,"] vs ",jugador2,"[",resultado2,"] ganador =>",ganador)
 }
-console.log("Resultado1 ", resultado1)
-console.log("Resultado2 ", resultado2)
+
+/// JUEGOS SEMIFINALES
+
+console.log("Empiezan Juegos Semifinales")
+console.log("===========================")
+
+//Juego 1
+let Semi_1=[]
+jugador1=Grupo_A[0]
+jugador2=Grupo_B[1]
+resultados=match()
+resultado1=resultados[0]
+resultado2=resultados[1]
+if (resultado1>resultado2){
+ganador=jugador1
+    Semi_1=[jugador1]
+} else {
+    ganador=jugador2
+    Semi_1 = [jugador2]}
+DesplegarJuego(jugador1,jugador2,resultado1,resultado2,ganador)
+
+//Juego 2
+let Semi_2=[]
+jugador1=Grupo_A[1]
+jugador2=Grupo_B[0]
+resultados=match()
+resultado1=resultados[0]
+resultado2=resultados[1]
+if (resultado1>resultado2){
+ganador=jugador1
+    Semi_2.push(jugador1)
+} else {
+    ganador=jugador2
+    Semi_2.push(jugador2)}
+DesplegarJuego(jugador1,jugador2,resultado1,resultado2,ganador)
+
+//Juego 3
+let Semi_3=[]
+jugador1=Grupo_C[0]
+jugador2=Grupo_D[1]
+resultados=match()
+resultado1=resultados[0]
+resultado2=resultados[1]
+if (resultado1>resultado2){
+    ganador=jugador1
+Semi_3=[jugador1]
+} else {
+    ganador=jugador2
+    Semi_3 = [jugador2]}
+DesplegarJuego(jugador1,jugador2,resultado1,resultado2,ganador)
 
 
+//Juego 4
+let Semi_4=[]
+jugador1=Grupo_C[1]
+jugador2=Grupo_D[0]
+resultados=match()
+resultado1=resultados[0]
+resultado2=resultados[1]
+if (resultado1>resultado2){
+    ganador=jugador1
+Semi_4.push(jugador1)
+} else {
+    ganador=jugador2
+    Semi_4.push(jugador2)}
+DesplegarJuego(jugador1,jugador2,resultado1,resultado2,ganador)
+
+
+
+//console.log("Resultado1 ", resultado1)
+//console.log("Resultado2 ", resultado2)
 
